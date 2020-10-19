b1d6bb416516132799680ba2425ed3d715e60701
Akulav <43496076+Akulav@users.noreply.github.com>
2020-10-19 21:44:03 +0300

    Initial release

---
+++ Hashed/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salt = "WlptaaWGLMjFdA==";
+            string hash = "aIJzUhcJxRLToXfMH7ozYX8UbUc622GqgUse78hlnOA=";
+            string input = Console.ReadLine();
+            if (Generator.AreEqual(input, hash, salt))
+            {
+                Console.WriteLine("Corect");
+            }
+
+            else { Console.WriteLine("Gresit"); }
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Generator
+    {
+        public static string CreateSalt(int size)
+        {
+
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public static string GenerateHash(string input, string salt)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            SHA256Managed sHA256ManagedString = new SHA256Managed();
+            byte[] hash = sHA256ManagedString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool AreEqual(string plainTextInput, string hashedInput, string salt)
+        {
+            string newHashedPin = GenerateHash(plainTextInput, salt);
+            return newHashedPin.Equals(hashedInput);
+        }
+    }
+}
